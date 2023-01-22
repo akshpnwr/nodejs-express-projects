51721b80b33efa0133eabf3505e1da8edbb98f53
Akash Panwar <akashpanwar9976@gmail.com>
2023-01-22 22:26:23 +0530

    Adding basic validation for post requests

--- task-manager/models/Task.js
+++ task-manager/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+const TaskSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name must be less than 20 character"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
