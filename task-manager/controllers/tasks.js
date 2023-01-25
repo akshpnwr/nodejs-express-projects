3deda8f1d03f22fed0aebaf37e4be942b6b68fde
Akash Panwar <akashpanwar9976@gmail.com>
2023-01-25 20:00:36 +0530

    Model.find and Model.findOne

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,7 +1,13 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({}); // Get all the data
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+    console.log(error);
+  }
 };
 
 const createTask = async (req, res) => {
@@ -13,9 +19,20 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  // res.send("get task");
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(500).json({ task });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
