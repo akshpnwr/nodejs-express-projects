60b16e0e366ddb22823a7cc5fdd91f99d0e73aa9
Akash Panwar <akashpanwar9976@gmail.com>
2023-02-06 20:10:17 +0530

    middleware wrapper added ->to remove try and catch

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,26 +1,33 @@
-const express = require("express");
-const app = express();
-const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect"); //Executes the mongoose.connect fn automatically
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect') //Executes the mongoose.connect fn automatically
+const notFound = require('./middleware/notFound')
 
-require("dotenv").config();
+require('dotenv').config()
 
 //Middleware
 
-app.use(express.static("./public"));
-app.use(express.json());
+app.use(express.static('./public'))
+app.use(express.json())
 
-app.use("/api/v1/tasks", tasks);
+app.use('/api/v1/tasks', tasks)
 
-const port = 3000;
+app.get('/hello', (req, res) => {
+  res.send('Hey, wat you doin')
+})
+
+app.use(notFound)
+
+const port = 3000
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listening on port ${port}...`));
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
